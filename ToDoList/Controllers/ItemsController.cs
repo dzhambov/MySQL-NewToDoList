fcef6e3ab8520cf846f616b235591d06f41408c3
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-18 10:31:22 -0700

    add create functionality with Entity, update ItemsController.cs, and Views/Items/Edit.cshtml, Views/Items/Details.cshtml
    Co-authored-by: Benjamin-Thom <jaminthom@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -32,8 +33,22 @@ namespace ToDoList.Controllers
     }
     public ActionResult Details(int id)
     {
-    Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-    return View(thisItem);
+      Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
